2fc33dceafb2c701bb6676880890a8a38eb39512
Wyz_Vy <santimol1121@gmail.com>
2023-11-17 02:19:11 -0500

    Pantallas nuevas.

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciarSesionComponent } from './autenticacion/iniciar-sesion/iniciar-sesion.component';
+import { RecuperarComponent } from './autenticacion/recuperar/recuperar.component';
+import { RegistrarseComponent } from './autenticacion/registrarse/registrarse.component';
+import { NotificarComponent } from './usuario/notificar/notificar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ingreso', component: IniciarSesionComponent},
+  {path:'', pathMatch:'full', component: IniciarSesionComponent},
+  {path:'recuperar', component: RecuperarComponent},
+  {path:'registro', component: RegistrarseComponent},
+  {path:'usuarionot', component: NotificarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/src/app/autenticacion/iniciar-sesion/iniciar-sesion.component.ts
+++ Angular/src/app/autenticacion/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrls: ['./iniciar-sesion.component.css']
 })
+
 export class IniciarSesionComponent {
+
+  constructor(private router: Router) {}
+
+  redirigirARegistro() {
+    this.router.navigate(['/registro']);
+  }
 
 }
