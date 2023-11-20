bc3038ea06d1a4b83541aaec2d2feed20cbf7980
sierrao0 <kamisops@gmail.com>
2023-11-19 22:17:19 -0500

    Redireccion desde login a los diferentes componentes

--- Angular/src/app/autenticacion/iniciar-sesion/iniciar-sesion.component.ts
+++ Angular/src/app/autenticacion/iniciar-sesion/iniciar-sesion.component.ts
@@ -9,7 +9,29 @@ import { Router } from '@angular/router';
 
 export class IniciarSesionComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
+
+  redirigirAInicio() {
+    const inputCorreo = document.getElementById('correo-electronico') as HTMLInputElement;
+    let domain = this.extractDomain(inputCorreo.value);
+    if (domain === "admin.com") {
+      this.router.navigate(['/administradorevent'])
+    } else if (domain === "user.com") {
+      this.router.navigate(['/userin'])
+    } else if (domain === "staff.com") {
+      this.router.navigate(['/personalin'])
+    }
+  }
+
+  extractDomain(email: string): string {
+    const parts = email.split('@');
+    if (parts.length === 2) {
+      return parts[1];
+    } else {
+      console.error('Invalid email format');
+      return '';
+    }
+  }
 
   redirigirARegistro() {
     this.router.navigate(['/registro']);
